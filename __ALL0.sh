6664583d757ceb50348e28571e5a92540726df20
fzampirolli <fzampirolli@gmail.com>
2022-02-22 16:54:16 -0300

    all exams

---
+++ __ALL0.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+source __all.sh 2012 10000
+source __all.sh 2011 10000
+source __all.sh 2010 10000
+source __all.sh 2009 10000
+source __all.sh 2008 10000
+source __all.sh 2007 10000
+source __all.sh 2006 10000
+source __all.sh 2005 10000
+source __all.sh 2004 10000
+source __all.sh 2003 10000
+source __all.sh 2002 10000
+source __all.sh 2001 10000
+source __all.sh 2000 10000
+source __all.sh 1999 10000
+source __all.sh 1998 10000
